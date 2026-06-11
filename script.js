7b41da4be1c08e35696ba1944bcf8e5180dbf50d
prakashabhijeet1-sudo <prakashabhijeet1@gmail.com>
2026-06-11 09:47:00 +0530

    touchup

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 const display = document.getElementById("display");
 
-function appendValue(value) {
-  display.value += value;
+function appendValue(value){
+    display.value += value;
 }
 
-function clearDisplay() {
-  display.value = "";
+function clearDisplay(){
+    display.value = "";
 }
 
-function deleteLast() {
-  display.value = display.value.slice(0, -1);
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
 }
 
-function calculate() {
-  try {
-    display.value = eval(display.value);
-  } catch (error) {
-    display.value = "Error";
-  }
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
 }
+
+document.addEventListener("keydown", (e) => {
+
+    const allowed = "0123456789+-*/.%";
+
+    if(allowed.includes(e.key)){
+        appendValue(e.key);
+    }
+
+    if(e.key === "Enter"){
+        calculate();
+    }
+
+    if(e.key === "Backspace"){
+        deleteLast();
+    }
+
+    if(e.key === "Escape"){
+        clearDisplay();
+    }
+});
